d041a0746017ed91ad303aa5b004db96f07b8b40
Luis-Marcelo-BS <Luisss_2000@hotmail.es>
2021-12-23 02:39:01 -0400

    update

---
+++ proyecto/html/p.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.x1',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '-300px'
+    });
+    sr.reveal('.logo',{
+        duration: 3000,
+        origin: 'button',
+        distance: '-300px'
+    });
+    sr.reveal('.co',{
+        duration: 5000,
+        origin: 'botton',
+        distance: '-100px'
+    });
+    sr.reveal('.co2',{
+        duration: 5000,
+        origin: 'left',
+        distance: '-400px'
+    });
+    sr.reveal('.co3',{
+        duration: 5000,
+        origin: 'button',
+        distance: '-400px'
+    });
+    sr.reveal('.img',{
+        duration:3000,
+        rotate:{
+            x:1,
+            y:180,
+            
+        }
+    });
+   
+    
